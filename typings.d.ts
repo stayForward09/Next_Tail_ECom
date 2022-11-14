c8e7d2ae288c8599692161343fe7b48700a4f9ee
AnichoKraus <anichokraus3123@gmail.com>
2022-11-14 15:54:14 -0800

    Add session interface

--- typings.d.ts
+++ typings.d.ts
@@ -11,3 +11,22 @@ export interface IProduct {
   };
   hasFast?: boolean;
 }
+
+export interface IOrder {
+  id: number;
+  amount: number;
+  amount_shipping: number;
+  items: { [key: string]: any };
+  timestamp: number;
+  images: string[];
+}
+
+export interface ISession {
+  user: {
+    name: string;
+    email: string;
+    image: string;
+    address: string;
+  } & DefaultSession["user"];
+  expires: string;
+}
